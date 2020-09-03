21f505c999cf8edf51e74f51411f3f0bc2536ae1
Jens <jens@weggemann.de>
2020-09-03 12:36:56 +0200

    Made the vulkan port a no-op. It doesn't work properly at the moment, see https://github.com/microsoft/vcpkg/issues/13331.

--- ports/vulkan/portfile.cmake
+++ ports/vulkan/portfile.cmake
@@ -3,6 +3,66 @@
 # Other packages can depend on this package to declare a dependency on Vulkan.
 include(vcpkg_common_functions)
 
+# Make this a no-op at the moment.
+SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+return()
+
+#>>>>
+# I'd like to get this closer to how FindVulkan.cmake works.
+# Due to the way this portfile is written, Vulkan detection fails
+# when installing the official Ubuntu packages. This probably affects
+# other distribution formats as well. The problem is that the Vulkan
+# SDK is not necessarily installed under the common root dir.
+# We should support that.
+# The strategy of FindVulkan.cmake is to use find_path and find_library
+# and pass it the assumed vulkan sdk root dir locations as hints.
+# If there is no vulkan root dir, this won't fail, but simply use the
+# normal search heuristics that presumably know common file layouts.
+
+# We don't need to be exact about finding the vulkan header
+# and library since we just want to check whether the SDK looks
+# like it is properly installed. Therefore we'll pass all the
+# hints for both Windows and Un*x in one go.
+
+set(VULKAN_ERROR_DL "Before continuing, please download and install Vulkan from:\n    https://vulkan.lunarg.com/sdk/home\nIf you have already downloaded it, make sure the VULKAN_SDK environment variable is set to vulkan's installation root.")
+
+if (OFF)
+message("===>")
+get_directory_property(tmp_var_names VARIABLES)
+foreach(tmp_var_name IN LISTS tmp_var_names)
+    message("${tmp_var_name} = \"${${tmp_var_name}}\"")
+endforeach()
+message("<===")
+endif()
+
+set(CMAKE_LIBRARY_ARCHITECTURE "x86_64-linux-gnu")
+
+file(TO_CMAKE_PATH "$ENV{VULKAN_SDK}" VULKAN_SDK_DIR)
+find_path(INCLUDE_FILE NAMES vulkan/vulkan.h PATHS
+    "${VULKAN_SDK_DIR}/Include"
+    "${VULKAN_SDK_DIR}/include"
+    "/usr/include")
+find_library(LIBRARY_FILE NAMES vulkan vulkan vulkan-1 PATHS 
+    "${VULKAN_SDK_DIR}/Lib"
+    "${VULKAN_SDK_DIR}/Lib32"
+    "${VULKAN_SDK_DIR}/Bin"
+    "${VULKAN_SDK_DIR}/Bin32"
+    "${VULKAN_SDK_DIR}/lib"
+    "/usr/lib/x86_64-linux-gnu")
+
+message(FATAL_ERROR "====> ${INCLUDE_FILE} || ${LIBRARY_FILE}")
+
+
+if (INCLUDE_FILE AND LIBRARY_FILE)
+    message(STATUS "Found Vulkan SDK version ${VULKAN_VERSION}")
+else()
+    message(FATAL_ERROR "Could not find Vulkan SDK. ${VULKAN_ERROR_DL}")
+endif()
+
+#<<<<
+
+if (OFF)
+
 message(STATUS "Querying VULKAN_SDK Enviroment variable")
 file(TO_CMAKE_PATH "$ENV{VULKAN_SDK}" VULKAN_DIR)
 set(VULKAN_INCLUDE "${VULKAN_DIR}/include/vulkan/")
@@ -39,6 +99,8 @@ elseif(EXISTS ${VULKAN_DIR}/LICENSE.txt)
     configure_file(${VULKAN_DIR}/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/vulkan/copyright COPYONLY)
 else()
     configure_file(${CURRENT_PORT_DIR}/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/vulkan/copyright COPYONLY)
+endif()
+
 endif()
 
 SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
